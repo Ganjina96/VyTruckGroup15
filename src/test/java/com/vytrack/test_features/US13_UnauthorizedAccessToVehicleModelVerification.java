e658d1bae6187bb318e2ff92bf019e1e10787725
AlexaSky <alexasky111@gmail.com>
2022-06-19 10:35:58 -0400

    Created a Driver utilities class

--- src/test/java/com/vytrack/test_features/US13_UnauthorizedAccessToVehicleModelVerification.java
+++ src/test/java/com/vytrack/test_features/US13_UnauthorizedAccessToVehicleModelVerification.java
@@ -3,6 +3,7 @@ package com.vytrack.test_features;
 
 import com.vytrack.base.TestBase;
 import com.vytrack.utilities.ConfigurationReader;
+import com.vytrack.utilities.Driver;
 import com.vytrack.utilities.VyTrack_Utilities;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
@@ -13,20 +14,21 @@ public class US13_UnauthorizedAccessToVehicleModelVerification extends TestBase 
     @Test
     public void Drivers_Cant_Access_AllVehicles_Models() throws InterruptedException {
         //        1. After logging into the website verify the title name is "Dashboard
-        driver.get(ConfigurationReader.getProperty("env"));
-        VyTrack_Utilities.vyTrackLogin(driver, ConfigurationReader.getProperty("TRUCK_DRIVER_1_LOGIN"), ConfigurationReader.getProperty("PASSWORD"));
+        Driver.getDriver().get(ConfigurationReader.getProperty("env"));
+        VyTrack_Utilities.vyTrackLogin(Driver.getDriver(), ConfigurationReader.getProperty("TRUCK_DRIVER_1_LOGIN"), ConfigurationReader.getProperty("PASSWORD"));
 
         //        2. Click on "Fleet" dropdown button
-        WebElement fleetDropdownBtn = driver.findElement(By.xpath("(//span[contains(@class, 'title-level-1')])[2]"));
+        WebElement fleetDropdownBtn = Driver.getDriver().findElement(By.xpath("(//span[contains(@class, 'title-level-1')])[2]"));
         fleetDropdownBtn.click();
         Thread.sleep(3000);
 
 //        3. Verify "Vehicles Model" option is displayed
-        WebElement vehiclesModel = driver.findElement(By.xpath("//span[.='Vehicles Model']"));
+        WebElement vehiclesModel = Driver.getDriver().findElement(By.xpath("//span[.='Vehicles Model']"));
         vehiclesModel.click();
         Thread.sleep(3000);
 
 //        4. Click and verify "Quick Launchpad" header is still displayed
-        Assert.assertEquals(driver.getTitle(), "Dashboard", "Title Verification Failed");
+        Assert.assertEquals(Driver.getDriver().getTitle(), "Dashboard", "Title Verification Failed");
+        Driver.closeDriver();
     }
 }
